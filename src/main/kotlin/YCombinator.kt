c9dbbcf22f2755a20c61bb243a106e7e68786460
Andrey Antipov <andrey.antipov@cxense.com>
2019-03-05 18:30:32 +0400

    remove unnecessary explicit types

--- src/main/kotlin/YCombinator.kt
+++ src/main/kotlin/YCombinator.kt
@@ -1,8 +1,8 @@
-private interface RF<F: Function1<*, *>> : (RF<F>) -> F
+private interface RF<F : Function1<*, *>> : (RF<F>) -> F
 
 fun <A, B> y(f: ((A) -> B) -> (A) -> B): (A) -> B {
-    val rf: RF<(A) -> B> = object : RF<(A) -> B> {
-        override fun invoke(w: RF<(A) -> B>): (A) -> B = f { a: A -> w(w)(a) }
+    val rf = object : RF<(A) -> B> {
+        override fun invoke(w: RF<(A) -> B>) = f { a -> w(w)(a) }
     }
 
     return rf(rf)
@@ -11,9 +11,9 @@ fun <A, B> y(f: ((A) -> B) -> (A) -> B): (A) -> B {
 fun main() {
     val factorial = y { f: (Int) -> Int -> { n -> if (n <= 1) 1 else n * f(n - 1) } }
     val fibonacci = y { f: (Int) -> Int -> { n -> if (n <= 2) 1 else f(n - 1) + f(n - 2) } }
-    print("Factorial(1..10)   : ")
-    (1..10).forEach { i -> print("${factorial(i)}  ") }
-    print("\nFibonacci(1..10)   : ")
-    (1..10).forEach { i -> print("${fibonacci(i)}  ") }
-    println()
+    val range = 1..10
+    print(
+            """Factorial[$range]   : ${range.map(factorial)}
+Fibonacci[$range]   : ${range.map(fibonacci)}
+""")
 }
