65208ea9b78765d6b6683ffb5dd9005c55de95ef
Andrey Antipov <andrey.antipov@cxense.com>
2019-03-05 21:45:32 +0400

    add docs to TCTrampoline

--- src/main/kotlin/tco/TCTrampoline.kt
+++ src/main/kotlin/tco/TCTrampoline.kt
@@ -1,29 +1,57 @@
+/**
+ * tco/TCTrampoline.kt
+ */
 package tco
 
 /**
- * @author Andrey Antipov (gorttar@gmail.com) (2018-04-09)
+ * specialized type of tail call optimizable functions
+ * @param T - function input type
+ * @param R - function output type
  */
 typealias TCFunction<T, R> = (T) -> TCResult<T, R>
 
-sealed class TCResult<in T, out R> {
-    abstract val fix: R
+/**
+ * wrapped result of [TCFunction]
+ * @param T - function input type
+ * @param R - function output type
+ */
+interface TCResult<in T, out R> {
+    /**
+     * raw result of [TCFunction]
+     * access will lead to [TCFunction] evaluation
+     */
+    val fix: R
 }
 
-private class Call<in T, out R>(private val a: T, private val nextStep: TCFunction<T, R>) : TCResult<T, R>() {
+/**
+ * implementation of intermediate [TCFunction] evaluation step
+ * @param T - function input type
+ * @param R - function output type
+ */
+private class Step<in T, out R>(private val t: T, private val nextStep: TCFunction<T, R>) : TCResult<T, R> {
     override val fix: R
         get() {
-            var step = next()
-            while (step is Call<T, R>) {
-                step = step.next()
-            }
+            var step = next
+            while (step is Step) step = step.next
             return step.fix
         }
 
-    private fun next(): TCResult<T, R> = nextStep(a)
+    private val next: TCResult<T, R> get() = nextStep(t)
 }
 
-private class Ret<out R>(override val fix: R) : TCResult<Any?, R>()
+/**
+ * tail call invocation of [TCFunction]
+ * creates next [Step] instead of actual evaluation
+ * @param T - function input type
+ * @param R - function output type
+ */
+infix operator fun <T, R> TCFunction<in T, out R>.get(t: T): TCResult<T, R> = Step(t, this)
 
-infix operator fun <A, B> TCFunction<A, B>.get(a: A): TCResult<A, B> = Call(a, this)
-
-val <R> R.ret: TCResult<Any?, R> get() = Ret(this)
+/**
+ * receiver wrapped as final [TCFunction] evaluation step
+ * @param R - function output type
+ */
+val <R> R.ret: TCResult<Any?, R>
+    get() = object : TCResult<Any?, R> {
+        override val fix: R = this@ret
+    }
